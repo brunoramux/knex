89bc42987b58da454b47b5799395515500f05846
brunoramux <bruno.lemos@live.com>
2022-12-01 20:14:13 -0300

    final

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,5 +6,9 @@ module.exports = {
     async index(req, res) {
         const results = await knex('users')
         return res.json(results)
+    },
+    async create(req, res) {
+        await knex('users').insert(req.body)
+        
     }
 }
